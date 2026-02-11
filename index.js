afb2d741baf4504b49a573aa531ed9f7c9fc1e21
danialzafar098 <abbasidani4466@gmail.com>
2026-02-11 16:38:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function MAKEHIMANGRY(){
+    document.getElementById("goku1").src = "images/goku2.png"
+}
+
+function unlockOMNIFORM(){
+    document.getElementById("goku2").src = "images/goku3.png"
+}
+
+function reset(){
+    document.getElementById("goku1").src = "images/goku1.png"
+     document.getElementById("goku2").src = "images/goku1.png"
+
+}
+
